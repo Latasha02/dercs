0e93a52e36ec655151e0d1691e7dbcf85a9c16ec
montira <montiraehpon99@gmail.com>
2021-06-04 22:51:05 +0800

    create repair service folder

--- BusinessServicesLayer/RepairServiceController/RepairServiceController.php
+++ BusinessServicesLayer/RepairServiceController/RepairServiceController.php
@@ -7,6 +7,59 @@ class RepairServiceController{
         $request = new RepairServiceModel();
         return $request->view();
     }
+
+    function delete(){
+        $data = new RepairServiceModel();
+        $data->RequestID = $_POST['RequestID'];
+        if($data->deleteRequest()){
+            $message = "Success Delete!";
+        echo "<script type='text/javascript'>alert('$message');
+        window.location = 'http://localhost/DERCS/ApplicationLayer/ManageRepairService/RequestList.php';</script>";
+        }
+    }
+
+    function viewRequest($RequestID){
+        $request = new RepairServiceModel();
+        $request->RequestID = $RequestID;
+        return $request->viewRequest();
+    }
+
+    function getName($CustomerID){
+        $request = new RepairServiceModel();
+        $request->CustomerID = $CustomerID;
+        return $request->getCustName();
+    }
+
+    function editStatus(){
+        $request = new RepairServiceModel();
+        $request->RequestID = $_POST['RequestID'];
+        $request->Request_Status = $_POST['Request_Status'];
+        $request->Request_Status = $_POST['Request_Status'];
+        $request->Reason = $_POST['Reason'];
+        $request->StaffID = $_POST['StaffID'];
+        if($request->updateStatus()){
+        echo "<script type='text/javascript'>;
+        window.location = 'http://localhost/DERCS/ApplicationLayer/ManageRepairService/RequestList.php';</script>";
+        }
+    }
+
+    function updateRequest(){
+        $request = new RepairServiceModel();
+        $request->RequestID = $_POST['RequestID'];
+        $request->Request_Status = $_POST['Request_Status'];
+        $request->Reason = $_POST['Reason'];
+        $request->Estimate_Cost = $_POST['Estimate_Cost'];
+       
+        
+        if($request->updateRequestStatus()){
+           $message = "Success Updated!";
+        echo "<script type='text/javascript'>alert('$message');
+       
+        window.location = 'http://localhost/DERCS/ApplicationLayer/ManageRepairService/RequestList.php';</script>";
+        }
+    }
+
+
     
     
 }
--- BusinessServicesLayer/RepairServiceModel/RepairServiceModel.php
+++ BusinessServicesLayer/RepairServiceModel/RepairServiceModel.php
@@ -2,7 +2,7 @@
 
 class RepairServiceModel{
 
-	function connect()
+    function connect()
     {
         $pdo = new PDO('mysql:host=localhost;dbname=sepdb', 'root', '');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -13,6 +13,49 @@ class RepairServiceModel{
         $sql = "select * from request";
         return RepairServiceModel::connect()->query($sql);;
     }
+
+    function deleteRequest(){
+        $sql = "delete from request where RequestID=:RequestID";
+        $args = [':RequestID'=>$this->RequestID];
+        $stmt = RepairServiceModel::connect()->prepare($sql);
+        $stmt->execute($args);
+        return $stmt;
+    }
+
+    function viewRequest(){
+        $sql = "select * from request where RequestID=:RequestID";
+        $args = [':RequestID'=>$this->RequestID];
+        $stmt = RepairServiceModel::connect()->prepare($sql);
+        $stmt->execute($args);
+        return $stmt;
+    }
+
+    function getCustName(){
+        $sql = "select * from customers where CustomerID=:CustomerID";
+        $args = [':CustomerID'=>$this->CustomerID];
+        $stmt = RepairServiceModel::connect()->prepare($sql);
+        $stmt->execute($args);
+        return $stmt;
+    }
+
+    function updateStatus()
+    {
+        $sql = "update request set Request_Status=:Request_Status, StaffID=:StaffID, Reason=:Reason where RequestID=:RequestID";
+        $args = [':RequestID'=>$this->RequestID,':Request_Status'=>$this->Request_Status,':StaffID'=>$this->StaffID,':Reason'=>$this->Reason];
+        $stmt = RepairServiceModel::connect()->prepare($sql);
+        $stmt->execute($args);
+        return $stmt;
+    }
+
+    function updateRequestStatus()
+    {
+        $sql = "update request set Request_Status=:Request_Status, Estimate_Cost=:Estimate_Cost, Reason=:Reason where RequestID=:RequestID";
+        $args = [':RequestID'=>$this->RequestID,':Request_Status'=>$this->Request_Status,':Estimate_Cost'=>$this->Estimate_Cost,':Reason'=>$this->Reason];
+        $stmt = RepairServiceModel::connect()->prepare($sql);
+        $stmt->execute($args);
+        return $stmt;
+    }
+    
 
  
 }
