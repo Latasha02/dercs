6a183ea29bce06657333e41c2554af23e045f429
hasya-manan <67544655+hasya-manan@users.noreply.github.com>
2021-06-11 13:04:29 +0800

    Update PaymentModel.php

--- BusinessServicesLayer/PaymentModel/PaymentModel.php
+++ BusinessServicesLayer/PaymentModel/PaymentModel.php
@@ -3,14 +3,14 @@
 class PaymentModel{
 
     public $Cust_Name,$Cust_PhoneNo,$Cust_Address,$PaymentType,$Time,$Device_Type,$Device_Model,$Defect_Type,$Estimate_Cost,$Request_Time;
-    
+    //connection to database
     function connect()
     {
         $pdo = new PDO('mysql:host=localhost;dbname=sepdb', 'root', '');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
     }
-
+//adding to database
     function addInformation(){
         $sql = "insert into payment(Cust_Name, Cust_PhoneNo, Cust_Address, PaymentType, Time) values(:Cust_Name, :Cust_PhoneNo, :Cust_Address, :PaymentType, :Time)";
 
@@ -20,18 +20,19 @@ class PaymentModel{
         return $stmt;
 
     }
-
+//display from db request
     function viewAll(){
          $sql = "select Device_Type,Device_Model,Defect_Type,Estimate_Cost from request";
          return PaymentModel::connect()->query($sql);;
 
     }
+    //display from db request
      function viewAtas(){
          $sql = "select Request_Time,Estimate_Cost from request";
          return PaymentModel::connect()->query($sql);;
 
     }
-
+//display from db request
     function viewBwh(){
          $sql = "select Device_Type,Device_Model,Defect_Type,Reason,Request_Time,Estimate_Cost from request";
          return PaymentModel::connect()->query($sql);;
