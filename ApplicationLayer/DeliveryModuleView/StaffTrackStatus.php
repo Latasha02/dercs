ec1874fcc638bbd83f9643172031da902395169f
Latasha02 <83638765+Latasha02@users.noreply.github.com>
2021-06-11 14:35:48 +0800

    csa
    done

--- ApplicationLayer/DeliveryModuleView/StaffTrackStatus.php
+++ ApplicationLayer/DeliveryModuleView/StaffTrackStatus.php
@@ -96,9 +96,10 @@ html,body,h1,h2,h3,h4,h5,h6 {font-family: "Roboto", sans-serif;}
     <i class="fa fa-remove"></i>
   </a>
   <h4 class="w3-bar-item"><b>Menu</b></h4>
-  <a class="w3-bar-item w3-button w3-hover-black" href="RiderPickupList.php">Pick up from Customer</a>
-  <a class="w3-bar-item w3-button w3-hover-black" href="RiderDeliveryList.php">Deliver to Customer List</a>
+  <a class="w3-bar-item w3-button w3-hover-black" href="../../ApplicationLayer/RepairServiceModule/RequestList.php">All Customer Request</a>
+  <a class="w3-bar-item w3-button w3-hover-black" href="#">Tracking</a>
   <a class="w3-bar-item w3-button w3-hover-black" href="#">My Profile</a>
+  
 </nav>
 
 <!-- Overlay effect when opening sidebar on small screens -->
